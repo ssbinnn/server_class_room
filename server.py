4a618b6f7b01c59b07673898be329c890dbd9f90
SuJeong <tnehd56@naver.com>
2023-12-01 16:37:30 +0900

    포기하자

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Hello, World!" #세상이 반갑지 않습니다
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
