221bef90e448b2572796e892c882794f0052a8af
ssbinnn <125570696+ssbinnn@users.noreply.github.com>
2023-12-08 15:05:52 +0900

    update server_socket

--- server_socket.py
+++ server_socket.py
@@ -6,14 +6,14 @@ def start_server(host, port, file_path):
     server_socket.bind((host, port))
     server_socket.listen(1)
 
-    print(f"서버가 시작되었습니다. 클라이언트 연결 대기 중...")
+    print("서버가 시작되었습니다. 클라이언트 연결 대기 중...")
 
     while True:
-        client_socket, addr = server_socket.accept()
-        print(f"클라이언트가 연결되었습니다: {addr}")
+        client_socket, addr = server_socket.accept() # 클라이언트와 연결
+        print("클라이언트가 연결되었습니다 : ", addr)
 
         while True:
-            # 클라이언트로부터 요청 받기
+            # 클라이언트로부터 요청(정보) 받기
             data = client_socket.recv(1024)
 
             # 클라이언트가 연결을 종료한 경우
@@ -25,25 +25,23 @@ def start_server(host, port, file_path):
             request = data.decode()
             print("받은 데이터 : ", request)
 
-            if (request):
-                # CSV 파일 읽기
-                request = request.strip('\n')
+            if (request): # 요청이 온 경우
+                request = request.strip('\n') # 받은 데이터에서 줄바꿈 기호 제거
 
-                with open(file_path, 'r', encoding='utf-8') as file:
+                with open(file_path, 'r', encoding='utf-8') as file: # csv 파일 읽기
                     reader = csv.reader(file)
                     next(reader)  # 헤더 건너뛰기
 
-                    for row in reader:
+                    for row in reader: # 행마다 확인
                         if row[0] == request:  # 교수 번호가 일치하면
-                            csv_data = ','.join(row)  # 해당 행의 데이터를 CSV 형식으로 변환
+                            csv_data = ','.join(row)  # 해당 행의 데이터를 쉼표로 구분해서 문자열 생성
                             csv_data_with_newline = csv_data + '\n' # 줄바꿈으로 데이터 끝임을 표기
 
-                            # 클라이언트에게 CSV 데이터 전송
-                            client_socket.sendall(csv_data_with_newline.encode())
+                            client_socket.sendall(csv_data_with_newline.encode()) # 데이터 전송
                             print("보낸 데이터 : ", csv_data)
                             break
 
-    server_socket.close()
+    server_socket.close() # 서버는 종료하지 않음
 
 if __name__ == "__main__":
     # 서버 호스트 및 포트 설정
