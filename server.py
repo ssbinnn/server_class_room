8d60f9459f107bd75acb72c8e10feae08db98696
ssbinnn <125570696+ssbinnn@users.noreply.github.com>
2024-05-19 19:39:48 +0900

    원상태

--- server.py
+++ server.py
@@ -5,13 +5,14 @@ import myhalfdirection as hdir
 import threading
 
 # 인원수 계산 #
-raspberry = 'http://192.168.161.249:8080/?action=stream'
+#raspberry = 'http://192.168.161.249:8080/?action=stream'
+raspberry = int(0)
 room = '113'
 #thread_direction = threading.Thread(target=dir.direction, args=(raspberry, room))
 thread_direction = threading.Thread(target=hdir.half_direction, args=(raspberry, room))
 thread_direction.start()
 
 ## 소켓 통신 서버 호스트 및 포트 설정 ##
-server_host = '192.168.161.158' # 와이파이 IP 주소
+server_host = '192.168.161.123' # 와이파이 IP 주소
 server_port = 5000
 soc.start_server(server_host, server_port)
